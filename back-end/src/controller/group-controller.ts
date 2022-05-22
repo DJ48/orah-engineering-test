fb23f2dab235c19d22662ea48f451139534aca6f
DJ48 <jaiswaldeepak264@gmail.com>
2022-05-22 14:01:12 +0530

    Added the CRUD operation for Group Api

--- back-end/src/controller/group-controller.ts
+++ back-end/src/controller/group-controller.ts
@@ -1,29 +1,70 @@
+import { getRepository } from "typeorm"
 import { NextFunction, Request, Response } from "express"
+import { Group } from "../entity/group.entity"
+import { CreateGroupInput } from "../interface/group.interface"
 
 export class GroupController {
+  private groupRepository = getRepository(Group)
 
   async allGroups(request: Request, response: Response, next: NextFunction) {
     // Task 1: 
-    
     // Return the list of all groups
+    return this.groupRepository.find()
   }
 
   async createGroup(request: Request, response: Response, next: NextFunction) {
     // Task 1: 
-    
     // Add a Group
+    const { body: params } = request
+
+    const createGroupInput: CreateGroupInput = {
+      name: params.name,
+      number_of_weeks: params.number_of_weeks,
+      roll_states: params.roll_states,
+      incidents: params.incidents,
+      ltmt: params.ltmt
+    }
+    const roll = new Group()
+    roll.prepareToCreate(createGroupInput)
+    return this.groupRepository.save(roll)
   }
 
   async updateGroup(request: Request, response: Response, next: NextFunction) {
     // Task 1: 
-    
     // Update a Group
+
+    const { body: params } = request
+
+    const groupToUpdate = await this.groupRepository.findOne(params.id)
+
+    if(groupToUpdate === undefined){
+      return {msg:"Invalid Id"}
+    }else{
+      groupToUpdate.id = params.id
+      groupToUpdate.name = params.name
+      groupToUpdate.number_of_weeks = params.number_of_weeks
+      groupToUpdate.roll_states = params.roll_states
+      groupToUpdate.incidents = params.incidents
+      groupToUpdate.ltmt = params.ltmt
+
+      return this.groupRepository.save(groupToUpdate)
+    }
   }
 
   async removeGroup(request: Request, response: Response, next: NextFunction) {
     // Task 1: 
-    
     // Delete a Group
+
+    const result = { msg : ""}
+  
+    let groupToRemove = await this.groupRepository.findOne(request.params.id)
+    if(groupToRemove === undefined){
+      result.msg="Invalid Id"
+    }else{
+      await this.groupRepository.remove(groupToRemove)
+      result.msg="Group Deleted Successfully"
+    }
+    return result
   }
 
   async getGroupStudents(request: Request, response: Response, next: NextFunction) {
